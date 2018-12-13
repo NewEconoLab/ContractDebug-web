9369c8b51b8169600cc5cd8ac580c88eb47f9ae6
vincentOpenSource <13916486874@163.com>
2018-12-13 15:13:23 +0800

    更改 line -1

--- src/pages/conact/Debug.ts
+++ src/pages/conact/Debug.ts
@@ -195,7 +195,7 @@ export default class Debug extends Vue
 
                 console.log("addr----------" + op.addr);
                 var line = this.addr.GetLineBack(op.addr);//尽量倒着取到对应的代码 codemirro 塞入的时候多往下了
-                var line = line > 1 ? line : 1;
+                line = line > 1 ? line : 1;
                 console.log("line----------" + line);
                 this.cEditor.setCursor(line);
                 this.cEditor.addLineClass(line, "background", "cursor-line-highight");
