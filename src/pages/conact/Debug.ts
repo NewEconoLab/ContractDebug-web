7c241e8b8d94ee0bc58023fe4fadc2c4be2848f7
vincentOpenSource <13916486874@163.com>
2018-12-13 15:14:46 +0800

    更改显示行数

--- src/pages/conact/Debug.ts
+++ src/pages/conact/Debug.ts
@@ -195,8 +195,8 @@ export default class Debug extends Vue
 
                 console.log("addr----------" + op.addr);
                 var line = this.addr.GetLineBack(op.addr);//尽量倒着取到对应的代码 codemirro 塞入的时候多往下了
-                line = line > 1 ? line : 1;
                 console.log("line----------" + line);
+                line = line > 1 ? line : 1;
                 this.cEditor.setCursor(line);
                 this.cEditor.addLineClass(line, "background", "cursor-line-highight");
                 this.currentHighlightLine = line;
